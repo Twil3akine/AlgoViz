e79f4ceb737388812bb32f939f3dbaeffa33c302
Twil3akine <twil3akine@gmail.com>
2024-12-03 22:30:01 +0900

    enclass segment_tree && visualize easiest

--- segment_tree/src/main.rs
+++ segment_tree/src/main.rs
@@ -7,35 +7,37 @@ struct Args {
     step_by_step: bool,
 }
 
-fn segment_tree(vec: &mut Vec<u32>, node: u32) -> () {
-    let mut rng = thread_rng();
-    let mut cnt: u32 = node;
-    let mut idx: u32 = 0;
-    while cnt>1 {
-        cnt /= 2;
-        idx += 1;
-    }
-    cnt = 2u32.pow(idx.into())-1u32;
+struct SegmentTree {
+    node: usize,
+    data: Vec<i32>,
+    default: i32,
+}
 
-    for _i in cnt..(2u32.pow(node.into())-1) {
-        vec.push(
-            if _i < cnt+node {
-                rng.gen_range(1..100)
-            } else {
-                0
-            }
-        );
-        let mut i: usize = _i as usize;
-        while i>0 {
-            if i%2==0 {
-                i -= 1;
-            }
-            i /= 2;
-            if vec[i] < vec[_i as usize] {
-                vec[i] = vec[_i as usize]
-            } else {
-                break;
-            }
+impl SegmentTree {
+    fn new(size: usize, default: i32) -> Self {
+        let node = size.next_power_of_two()-1;
+        SegmentTree {
+            node,
+            data: vec![default; 2*node+1],
+            default,
+        }
+    }
+
+    fn build(&mut self, vals: &[i32]) { // val: &slice <i32>
+        for (i, &v) in vals.iter().enumerate() {
+            self.data[self.node+i] = v;
+        }
+        for i in (0..self.node).rev() {
+            self.data[i] = self.data[2*(i+1)-1].max(self.data[2*(i+1)]);
+        }
+    }
+
+    fn update(&mut self, mut idx: usize, val: i32) {
+        idx += self.node;
+        self.data[idx] = val;
+        while idx > 0 {
+            idx /= 2;
+            self.data[idx] = self.data[2*(idx+1)-1].max(self.data[2*(idx+1)]);
         }
     }
 }
@@ -44,29 +46,35 @@ fn main() {
     let mut rng = thread_rng();
     let args = Args::parse();
 
-    let node: u32 = rng.gen_range(4..=16);
-    let mut vec: Vec<u32> = vec!(0; 15);
+    let node: u32 = rng.gen_range(9..=16);
+    let mut vec: Vec<i32> = Vec::new();
+    
+    for _ in 0..node {
+        vec.push(rng.gen_range(1..100));
+    }
 
-    segment_tree(&mut vec, node);
+    let mut segment_tree = SegmentTree::new(16, 0);
+
+    segment_tree.build(&vec);
 
     for i in 0..1 as usize {
-        print!("                               {:>2}                               ", vec[i]);
+        print!("                               {:>2}                               ", segment_tree.data[i]);
     }
     println!();
     for i in 1..3 as usize {
-        print!("               {:>2}               ", vec[i]);
+        print!("               {:>2}               ", segment_tree.data[i]);
     }
     println!();
     for i in 3..7 as usize {
-        print!("       {:>2}       ", vec[i]);
+        print!("       {:>2}       ", segment_tree.data[i]);
     }
     println!();
     for i in 7..15 as usize {
-        print!("   {:>2}   ", vec[i]);
+        print!("   {:>2}   ", segment_tree.data[i]);
     }
     println!();
     for i in 15..31 as usize {
-        print!(" {:>2} ", vec[i]);
+        print!(" {:>2} ", segment_tree.data[i]);
     }
     println!();
 }
